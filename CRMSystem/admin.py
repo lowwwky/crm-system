0c5fd06f2e0560d3676b493e5d4d76d76c101a15
Любовь Кужлева <lu.kuzhlv@gmail.com>
2026-08-11 00:52:55 +0500

    first upload. ready: - database and tables (basic models) - templates for website - requirements.txt - gitignore - django connection and integration(?)

---
+++ CRMSystem/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import HRCalendar, HireCard, EmployeeCard
+
+admin.site.register(HRCalendar)
+
+admin.site.register(HireCard)
+
+admin.site.register(EmployeeCard)
